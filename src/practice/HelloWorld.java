88e928cb2e9536e24155771bc385cc8757ac14f2
Edward Hyunwoo Shin <shyunuya@gmail.com>
2017-08-04 12:53:24 +0900

    Initial commit

---
+++ src/practice/HelloWorld.java
@@ -0,0 +1,11 @@
+package practice;
+
+/**
+ * Created by Edward-PC on 2017-08-04.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
+
