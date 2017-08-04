11d395379880ebf46b227d4e03139cc827db6806
Edward Hyunwoo Shin <shyunuya@gmail.com>
2017-08-04 12:59:01 +0900

    basic algorithms start

---
+++ src/algorithms/Basic_Algorithm.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+/**
+ * Created by Edward-PC on 2017-08-04.
+ */
+public class Basic_Algorithm {
+    public static void main(String[] args) {
+        System.out.println("Basic Algorithm practice");
+    }
+}
