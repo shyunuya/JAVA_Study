46ff7a68d1f87a2b456d716ef82f9eb807810b0a
Edward Hyunwoo Shin <shyunuya@gmail.com>
2017-08-07 12:39:46 +0900

    basic oop practice start

---
+++ src/oop_practice/OopPractice.java
@@ -0,0 +1,10 @@
+package oop_practice;
+
+/**
+ * Created by Edward-PC on 2017-08-07.
+ */
+public class OopPractice {
+    public static void main(String[] args) {
+        System.out.println("Object-Oriented Programming Practice");
+    }
+}
