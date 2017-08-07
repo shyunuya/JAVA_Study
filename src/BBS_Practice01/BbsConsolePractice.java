933a9b3111c3fa0b1bef7578e02d0c074006e96b
Edward Hyunwoo Shin <shyunuya@gmail.com>
2017-08-07 12:55:33 +0900

    Java Console BBS practice start

---
+++ src/BBS_Practice01/BbsConsolePractice.java
@@ -0,0 +1,10 @@
+package BBS_Practice01;
+
+/**
+ * Created by Edward-PC on 2017-08-07.
+ */
+public class BbsConsolePractice {
+    public static void main(String[] args) {
+        System.out.println("Java Console Bulletin Board System Practice Start");
+    }
+}
